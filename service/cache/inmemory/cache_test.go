ce0452f4185ec5481d50426f5f8ce27a9ba19a48
Radoslaw Krzesniak <radoslaw.krzesniak@gmail.com>
2020-01-24 14:55:59 +0100

    test refactoring

--- service/cache/inmemory/cache_test.go
+++ service/cache/inmemory/cache_test.go
@@ -1,41 +1,124 @@
 package inmemory
 
 import (
+	"reflect"
 	"testing"
 	"time"
-
-	"gotest.tools/assert"
 )
 
-func TestAreElementCached(t *testing.T) {
+func Test_cacheService_Get(t *testing.T) {
 
-	sut := New(5)
-
-	_, found := sut.Get("abc")
-
-	assert.Equal(t, found, false)
+	sut := New(10).(*cacheService)
 
 	sut.Set("abc", 123)
-	m, found := sut.Get("abc")
 
-	assert.Equal(t, found, true)
-	assert.Equal(t, m, 123)
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name      string
+		c         *cacheService
+		args      args
+		wantValue interface{}
+		wantFound bool
+	}{
+		{
+			name: "test not found",
+			c:    sut,
+			args: args{
+				key: "xyz",
+			},
+			wantValue: nil,
+			wantFound: false,
+		},
+		{
+			name: "test found",
+			c:    sut,
+			args: args{
+				key: "abc",
+			},
+			wantValue: 123,
+			wantFound: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotValue, gotFound := tt.c.Get(tt.args.key)
+			if !reflect.DeepEqual(gotValue, tt.wantValue) {
+				t.Errorf("cacheService.Get() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+			if gotFound != tt.wantFound {
+				t.Errorf("cacheService.Get() gotFound = %v, want %v", gotFound, tt.wantFound)
+			}
+		})
+	}
 }
 
-func TestTimeoutElapsed(t *testing.T) {
+func Test_cacheService_Get_timeout_elapsed(t *testing.T) {
 
-	sut := New(1)
+	sut := New(1).(*cacheService)
 
 	sut.Set("abc", 123)
-
-	_, found := sut.Get("abc")
-
-	assert.Equal(t, found, true)
-
 	time.Sleep(2 * time.Second)
 
-	_, found = sut.Get("abc")
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name      string
+		c         *cacheService
+		args      args
+		wantValue interface{}
+		wantFound bool
+	}{
+		{
+			name: "cache item timeout",
+			c:    sut,
+			args: args{
+				key: "abc",
+			},
+			wantValue: nil,
+			wantFound: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotValue, gotFound := tt.c.Get(tt.args.key)
+			if !reflect.DeepEqual(gotValue, tt.wantValue) {
+				t.Errorf("cacheService.Get() gotValue = %v, want %v", gotValue, tt.wantValue)
+			}
+			if gotFound != tt.wantFound {
+				t.Errorf("cacheService.Get() gotFound = %v, want %v", gotFound, tt.wantFound)
+			}
+		})
+	}
+}
 
-	assert.Equal(t, found, false)
+func Test_cacheService_Set(t *testing.T) {
 
+	sut := New(10).(*cacheService)
+
+	type args struct {
+		key   string
+		value interface{}
+	}
+	tests := []struct {
+		name string
+		c    *cacheService
+		args args
+	}{
+		{
+			name: "set value",
+			c:    sut,
+			args: args{
+				key:   "abc",
+				value: 123,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.c.Set(tt.args.key, tt.args.value)
+		})
+	}
 }
